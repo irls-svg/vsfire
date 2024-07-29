297c07d651c830dd1837383d42dafade284e21d0
lillian <lillianfk@proton.me>
2024-07-29 23:09:04 +1000

    fix: jest conflict with vscode-test

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+.tsx?$': ['ts-jest', {}],
-  },
-  // Jest shouldn't try to run /test/ folder tests
-  testRegex: __dirname + '/src/.*\\.test\\.tsx?$'
-};
